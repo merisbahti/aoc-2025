a83839fa57430c24a74ca9b00521886d117c1670
meris <meris.bahti@gmail.com>
2025-12-01 12:27:51 +0100

    sol2

--- src/aoc_2025/day1.clj
+++ src/aoc_2025/day1.clj
@@ -1,5 +1,6 @@
 (ns aoc-2025.day1
   (:require
+   [clojure.math :as math]
    [clojure.string :as str]
    [clojure.test :refer [is testing deftest]]
    [aoc-2025.core :refer [get-input-for-day]]))
@@ -26,11 +27,58 @@ L82")
                                   (cons (mod (+ curr head) 100) (cons head tail))) [50])
                         (filter (partial = 0)) (count)))
 
-(defn sol2 [input] nil)
+(defn sol2 [input] (->> (str/split input #"\n")
+                        (map
+                         (fn [row]
+                           (let [[_ polarity count] (re-find (re-matcher #"([LR])(\d+)" row))]
+                             (* (if (= polarity "R") 1 -1) (parse-long count)))))
+
+                        (reduce
+                         (fn [{latest :latest zeros :zeros} curr]
+                           (let* [sum (+  latest curr)
+                                  new-latest (mod sum 100)
+
+                                  new-zeros (+ zeros  (count-zeros latest curr))
+
+                                  _ (println {:latest latest :curr curr :sum sum :new-latest new-latest
+
+                                              :new-zeros new-zeros})]
+                                 {:latest new-latest
+                                  :zeros new-zeros}))
+
+                         {:latest 50 :zeros 0})
+                        (:zeros)))
+
+(mod -10 100)
+
+(defn count-zeros [start diff]
+  (let [factor (if (neg? diff) -1 1)]
+    (loop [curr start
+           remaining (abs diff)
+           zeros 0]
+      (let* [new-curr (mod (+ curr factor) 100)]
+            (if (= remaining 0) zeros
+                (recur new-curr
+                       (- remaining 1)
+                       (+ zeros (if (= new-curr 0) 1 0))))))))
 
 (deftest input-tests
   (testing "part 1"
     (is (= 3 (sol1 testinput)))
     (is (= 1105 (sol1 input)))
-    (is (= (sol2 testinput) nil))
-    (is (= (sol2 input) nil))))
+    (is (= 6 (sol2 testinput)))
+    (is (= 6599 (sol2 input)))))
+(deftest count-zero-test
+  (testing "stuff"
+    (is (= 1 (count-zeros 50 -83)))
+    (is (= 10 (count-zeros 50 1049)))
+    (is (= 11 (count-zeros 50 1050)))
+    (is (= 0 (count-zeros 50 49)))
+    (is (= 0 (count-zeros 98 1)))
+    (is (= 0 (count-zeros 99 0)))
+    (is (= 0 (count-zeros 82 -30)))
+    (is (= 0 (count-zeros 0 -1)))
+    (is (= 0 (count-zeros 0 1)))
+    (is (= 1 (count-zeros 1 -1)))
+    (is (= 1 (count-zeros 99 1)))
+    (is (= 1 (count-zeros 95 60)))))
