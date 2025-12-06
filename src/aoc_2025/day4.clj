bb516aedbb8f435abe6861f5a9587403b3b3c47d
meris <meris.bahti@gmail.com>
2025-12-06 06:51:42 +0100

    p1

--- src/aoc_2025/day4.clj
+++ src/aoc_2025/day4.clj
@@ -16,12 +16,44 @@
 @.@.@@@.@.")
 (def input (get-input-for-day))
 
-(defn sol1 [input] nil)
+(def neighbor-deltas
+  [[-1 -1] [-1 0] [-1 1]
+   [0 -1]        [0 1]
+   [1 -1] [1 0] [1 1]])
+
+(defn count-neighbors [[x y] full]
+  {:pre [(set? full)]} ;; :pre is the optimized way to assert in Clojure
+  (->> neighbor-deltas
+       (filter (fn [[dx dy]]
+                 ;; FAST: direct hash lookup
+                 ;; This asks: "Does 'full' contain this coordinate?"
+                 (full [(+ x dx) (+ y dy)])))
+       count))
+
+(comment (count-neighbors [0 0] (into #{} [[0 1] [0 2] [0 -1] [0 0]]))
+         (count-neighbors [0 0] (into #{} [[-1 -1] [0 -1] [1 -1]
+                                           [-1 0] [0 0] [1 0]
+                                           [-1 1] [0 1] [1 1]])))
+
+(defn sol1 [input]
+  (as->
+   (->> (str/split input #"\n")
+        (map #(str/split % #""))
+        (map-indexed
+         (fn [y row]
+           (map-indexed (fn [x col] (when (= "@" col) [x y])) row)))
+        (reduce into #{})
+        (filter (complement nil?))
+        (set)) x
+    (map #(time (count-neighbors % x)) x)
+    (filter #(< % 4) x)
+    (count x)))
+
 (defn sol2 [input] nil)
 
 (deftest input-tests
   (testing "part 1"
-    (is (= (sol1 testinput) nil))
-    (is (= (sol1 input) nil))
+    (is (= (sol1 testinput) 13))
+    (is (= (sol1 input) 1428))
     (is (= (sol2 testinput) nil))
     (is (= (sol2 input) nil))))
