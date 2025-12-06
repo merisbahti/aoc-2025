8de7eed63b9af4fc5a45756a82c6a89c8f8483b2
meris <meris.bahti@gmail.com>
2025-12-06 05:40:58 +0100

    change template

--- src/aoc_2025/template.clj
+++ src/aoc_2025/template.clj
@@ -1,8 +1,10 @@
 (ns aoc-2025.template
   (:require
-   [clojure.test :refer [is testing deftest]]
-   [aoc-2025.core :refer [get-input-for-day]]))
+   [aoc-2025.core :refer [get-input-for-day]]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is testing]]))
 
+(def testinput "")
 (def input (get-input-for-day))
 
 (defn sol1 [input] nil)
@@ -10,5 +12,7 @@
 
 (deftest input-tests
   (testing "part 1"
+    (is (= (sol1 testinput) nil))
     (is (= (sol1 input) nil))
+    (is (= (sol2 testinput) nil))
     (is (= (sol2 input) nil))))
