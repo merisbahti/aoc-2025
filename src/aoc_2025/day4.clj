562ad37aacad94829048a147a4a5ca915c7525a5
meris <meris.bahti@gmail.com>
2025-12-06 05:43:14 +0100

    init d4

---
+++ src/aoc_2025/day4.clj
@@ -0,0 +1,27 @@
+(ns aoc-2025.day4
+  (:require
+   [aoc-2025.core :refer [get-input-for-day]]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is testing]]))
+
+(def testinput "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.")
+(def input (get-input-for-day))
+
+(defn sol1 [input] nil)
+(defn sol2 [input] nil)
+
+(deftest input-tests
+  (testing "part 1"
+    (is (= (sol1 testinput) nil))
+    (is (= (sol1 input) nil))
+    (is (= (sol2 testinput) nil))
+    (is (= (sol2 input) nil))))
