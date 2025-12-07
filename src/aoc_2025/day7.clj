17c48ca5609006fd86b632f4dc38d96a5da9fca4
meris <meris.bahti@gmail.com>
2025-12-07 18:51:44 +0100

    refac

--- src/aoc_2025/day7.clj
+++ src/aoc_2025/day7.clj
@@ -23,38 +23,39 @@
 
 (def input (get-input-for-day))
 
-(defn sol1 [input] (->> (str/split input #"\n")
-                        (map
-                         (fn [row]
-                           (->> (str/split row #"")
-                                (map-indexed (fn [i x] [i x]))
-                                (filter (fn [[_ x]] (or  (= x "S") (= x "^")))))))
-                        (reduce (fn [{tachyons :tachyons split-beams :split-beams} row]
-                                  (assert (every?
+(defn sol1 [input]
+  (->> (str/split input #"\n")
+       (map
+        (fn [row]
+          (->> (str/split row #"")
+               (map-indexed (fn [i x] [i x]))
+               (filter (fn [[_ x]] (or  (= x "S") (= x "^")))))))
+       (reduce (fn [{tachyons :tachyons split-beams :split-beams} row]
+                 (assert (every?
 
-                                           (fn [[_ x]]
-                                             (or (= x "S") (= x  "^")))
-                                           row))
-                                  (assert (every? number? tachyons))
-                                  (let [splitter-indexes (set (map first row))
-                                        new-split-tachyons
-                                        (count (filter (fn [tachyon] (splitter-indexes tachyon)) tachyons))
-                                        new-tachyons
-                                        (concat
-                                         (mapcat (fn [[i x]] (if (= "S" x) [i] []))
-                                                 row)
-                                         (mapcat (fn [tachyon-index]
-                                                   (cond
-                                                     (splitter-indexes tachyon-index)
-                                                     [(- tachyon-index 1) (+ tachyon-index 1)]
-                                                     :else [tachyon-index]))
+                          (fn [[_ x]]
+                            (or (= x "S") (= x  "^")))
+                          row))
+                 (assert (every? number? tachyons))
+                 (let [splitter-indexes (set (map first row))
+                       new-split-tachyons
+                       (count (filter (fn [tachyon] (splitter-indexes tachyon)) tachyons))
+                       new-tachyons
+                       (concat
+                        (mapcat (fn [[i x]] (if (= "S" x) [i] []))
+                                row)
+                        (mapcat (fn [tachyon-index]
+                                  (cond
+                                    (splitter-indexes tachyon-index)
+                                    [(- tachyon-index 1) (+ tachyon-index 1)]
+                                    :else [tachyon-index]))
 
-                                                 tachyons))]
+                                tachyons))]
 
-                                    {:split-beams (+ split-beams new-split-tachyons) :tachyons (set new-tachyons)}))
+                   {:split-beams (+ split-beams new-split-tachyons) :tachyons (set new-tachyons)}))
 
-                                {:split-beams 0 :tachyons #{}})
-                        (:split-beams)))
+               {:split-beams 0 :tachyons #{}})
+       (:split-beams)))
 (defn sol2 [input] nil)
 
 (deftest input-tests
