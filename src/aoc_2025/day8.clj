79ba0720e56a3a05fe6e27dd3705db37c1fc1e3d
meris <meris.bahti@gmail.com>
2025-12-10 05:26:44 +0100

    fix d8p2

--- src/aoc_2025/day8.clj
+++ src/aoc_2025/day8.clj
@@ -57,36 +57,6 @@
     (is (= [#{1 2}]					 (connect-circuits 1 2 [#{2} #{1}])))
     (is (= [#{1 2 3 5 7 8}]					 (connect-circuits 1 2 [#{2 7 8} #{1 3 5}])))))
 
-(time
- (print (->>
-         (str/split input #"\n")
-         (map #(map parse-long (str/split %  #",")))
-         ((fn [points]
-            (let [point-distances
-                  (->>
-                   (combo/combinations points 2)
-                   (map (fn [[p1 p2]] [(dist p1 p2) p1 p2]))
-                   (sort-by first))
-                  max-iters (case (count points)
-                              20 20
-                              1000 1000)]
-
-              (loop [iters 0
-                     circuits (map (fn [point] (set [point])) points)
-                     [[_ min-p1 min-p2] & remaining-point-distances] point-distances]
-
-                (if (>= iters max-iters)
-                  circuits
-                  (let [new-circuits
-                        (connect-circuits min-p1 min-p2 circuits)]
-
-                    (recur (+ 1 iters)
-                           new-circuits
-                           remaining-point-distances)))))))
-         (map count)
-         (sort-by -)
-         (take 3))))
-
 (assert (= (dist [1 1 0] [2 2 0]) (math/sqrt 2)))
 (assert (= (dist [1 1 1] [2 2 2]) (math/sqrt 3)))
 (assert (= (dist [1 2 3] [2 3 4]) (math/sqrt 3)))
@@ -140,8 +110,9 @@
 
           (let [new-circuits
                 (connect-circuits min-p1 min-p2 circuits)]
-            (if (= 2 (count circuits))
-              [min-p1 min-p2]
+
+            (if (= 1 (count new-circuits))
+              (* (first min-p1) (first min-p2))
               (recur (+ 1 iters)
                      new-circuits
                      remaining-point-distances)))))))))
@@ -150,5 +121,5 @@
   (testing "part 1"
     (is (= 40 (sol1 testinput)))
     (is (= 122430 (sol1 input)))
-    (is (= nil (sol2 testinput)))
-    (is (= nil (sol2 input)))))
+    (is (= 25272 (sol2 testinput)))
+    (is (= 8135565324 (sol2 input)))))
