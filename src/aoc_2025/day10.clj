b1c1696160a10c4ca3f59cba336cce1d79611640
meris <meris.bahti@gmail.com>
2025-12-10 12:19:12 +0100

    add inputs

--- src/aoc_2025/day10.clj
+++ src/aoc_2025/day10.clj
@@ -4,7 +4,9 @@
    [clojure.string :as str]
    [clojure.test :refer [deftest is testing]]))
 
-(def testinput "")
+(def testinput "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}")
 (def input (get-input-for-day))
 
 (defn sol1 [input] nil)
