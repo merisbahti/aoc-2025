9c3c9b0bb7f17bcf2fd58c8f86737a6c8ffcf217
meris <meris.bahti@gmail.com>
2026-01-10 13:31:47 +0100

    fix stuff

--- src/aoc/y2015/day7.clj
+++ src/aoc/y2015/day7.clj
@@ -11,6 +11,7 @@ x AND y -> d
 x OR y -> e
 x LSHIFT 2 -> f
 y RSHIFT 2 -> g
+y -> o
 NOT x -> h
 NOT y -> i")
 
@@ -19,24 +20,29 @@ NOT y -> i")
 (def input (get-input-for-day))
 
 (defn sol1 [input to-eval]
-  (let [exprs (->>
+  (let [eval-cache (atom {})
+        exprs (->>
                (str/split-lines input)
                (mapv (fn [line]
                        (->> (str/split line #" -> ")
                             (reverse)
                             (into []))))
                (into {}))
-        eval-expr (memoize
-                   (fn eval-expr [expr]
-                     (prn "evaling " expr)
-                     (condp re-matches expr
-                       #"^(\d+)$" :>> (fn [[_ _]] (parse-long expr))
-                       #"^([a-z]+)$" :>> (fn [[_ _]] (eval-expr (exprs expr)))
-                       #"(\w+)\s+AND\s+(\w+)" :>> (fn [[_ a b]] (bit-and (eval-expr a) (eval-expr b)))
-                       #"(\w+)\s+LSHIFT\s+(\w+)" :>> (fn [[_ a b]] (bit-shift-left (eval-expr a) (eval-expr b)))
-                       #"(\w+)\s+RSHIFT\s+(\w+)" :>> (fn [[_ a b]] (bit-shift-right (eval-expr a) (eval-expr b)))
-                       #"NOT\s+(\w+)" :>> (fn [[_ a]] (flip-all (eval-expr a)))
-                       #"(\w+)\s+OR\s+(\w+)" :>> (fn [[_ a b]] (bit-or (eval-expr a) (eval-expr b))))))]
+        eval-expr (fn eval-expr [expr]
+                    (let [cached (@eval-cache expr)]
+                      (if
+                       cached
+                        cached
+                        (let [evaled (condp re-matches expr
+                                       #"(\w+)\s+AND\s+(\w+)" :>> (fn [[_ a b]] (bit-and (eval-expr a) (eval-expr b)))
+                                       #"(\w+)\s+LSHIFT\s+(\w+)" :>> (fn [[_ a b]] (bit-shift-left (eval-expr a) (eval-expr b)))
+                                       #"(\w+)\s+RSHIFT\s+(\w+)" :>> (fn [[_ a b]] (bit-shift-right (eval-expr a) (eval-expr b)))
+                                       #"NOT\s+(\w+)" :>> (fn [[_ a]] (flip-all (eval-expr a)))
+                                       #"(\w+)\s+OR\s+(\w+)" :>> (fn [[_ a b]] (bit-or (eval-expr a) (eval-expr b)))
+                                       #"^(\d+)$" :>> (fn [[_ _]] (parse-long expr))
+                                       #"^([a-zA-Z]+)$" :>> (fn [[_ _]] (eval-expr (exprs expr))))]
+                          (swap! eval-cache (fn [map] (assoc map expr evaled)))
+                          evaled))))]
 
     (eval-expr to-eval)))
 (defn sol2 [input] nil)
@@ -49,8 +55,8 @@ NOT y -> i")
     (is (= 492 (sol1 testinput "f")))
     (is (= 114 (sol1 testinput "g")))
     (is (= 72 (sol1 testinput "d")))
+    (is (= 456 (sol1 testinput "o")))
     (is (= 507 (sol1 testinput "e")))
-
-    ;; (is (= nil (sol1 input)))
+    (is (= 956 (sol1 input "a")))
     (is (= nil (sol2 testinput)))
     (is (= nil (sol2 input)))))
